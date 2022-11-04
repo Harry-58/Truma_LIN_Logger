5278e2ac08071d9c03058b347d9802fca306664d
Harry-58 <56926733+Harry-58@users.noreply.github.com>
2022-11-04 10:54:13 +0100

    PROGMEM ergänzt weil zu hoher RAM-Verbrauch

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,33 @@
 #include <truma.h>
 
 /* LIN PACKET:
-   It consist of:
+   Aufbau:
     ___________ __________ _______ ____________ _________
    |           |          |       |            |         |
-   |Synch Break|Synch Byte|ID byte| Data Bytes |Checksum |
+   |Synch Break|Sync Byte |ID byte| Data Bytes |Checksum |
    |___________|__________|_______|____________|_________|
 
-   Every byte have start bit and stop bit and it is send LSB first.
-   Synch Break - 13 bits of dominant state ("0"), followed by 1 bit recesive state ("1")
-   Synch Byte - Byte for Bound rate syncronization, always 0x55
-   ID Byte - consist of parity, length and address; parity is determined by LIN standard and depends from address and message length
-   Data Bytes - user defined; depend on devices on LIN bus
-   Checksum - inverted 256 checksum; data bytes are sumed up and then inverted
+   Jedes Byte hat ein Startbit und ein Stoppbit und zuerst wird das LSB gesendet.
+   Synch Break - 13 Bits dominanter Zustand ("0"), gefolgt von 1 Bit rezessivem Zustand ("1")
+   Synch Byte  - Byte für Baud-Rate Syncronisation, immer 0x55
+   ID Byte     - bestehend aus Parität, Länge und Adresse; die Parität wird durch den LIN-Standard bestimmt und hängt von der Adresse und der Nachrichtenlänge ab
+   Datenbytes  - benutzerdefiniert; abhängig von den Geräten am LIN-Bus
+   Prüfsumme   - invertierte 256er Prüfsumme; Datenbytes werden summiert und dann invertiert
+
+
+   https://github.com/mestrode/Lin-Interface-Library/blob/main/src/Lin_Interface.cpp
 */
-// https://github.com/mestrode/Lin-Interface-Library/blob/main/src/Lin_Interface.cpp
 
 // LIN serial Interface
 const uint16_t linSpeed = 9600;  // speed LIN of IBS-Sensor (do not change)
 const int16_t linRX     = 2;     // rot    13;   // RX Pin LIN serial
 const int16_t linTX     = 3;     // orange 12;   // TX Pin LIN serial
 
-//#define test
+ #define test
 #ifndef test
-  SoftwareSerial linSerial(linTX, linRX);  // RX, TX
+SoftwareSerial linSerial(linTX, linRX);  // RX, TX
 #else
-  #define linSerial Serial
+#define linSerial Serial
 #endif
 
 byte LinMessage[9] = {0};
@@ -45,13 +47,14 @@ uint8_t ProtectedID;
 uint8_t ID;
 bool ChecksumValid;
 int8_t bytes_received = -4;
-bool decode=true;
+uint16_t logMode      = 0;
 
 //------------------------------------------
 void setup();
 void loop();
 bool linLoop();
 void linDecode();
+void linLoop2();
 void serialCommand();
 uint8_t getChecksum(uint8_t ProtectedID, uint8_t dataLen);
 template <typename... T>
@@ -67,15 +70,22 @@ void setup() {
 }
 
 void loop() {
-  if (linLoop()) {
-    if (decode) linDecode();
+  switch (logMode) {
+    case 0:
+      if (linLoop()) linDecode();
+      break;
+    case 1:
+      linLoop();
+      break;
+    case 2:
+      linLoop2();
+      break;
   }
-  #ifndef test
-   serialCommand();
-   #endif
+
+#ifndef test
+  serialCommand();
+#endif
 }
-
-
 
 bool linLoop() {
   while (linSerial.available()) {
@@ -142,10 +152,20 @@ bool linLoop() {
 void linDecode() {
   switch (ID) {
     case 0x20:
-      tRoom  = float(LinMessage[0] + ((LinMessage[1] & 0x0F) << 8)) / 10.0 - 273;         // Room temperature [degC]
-      tWater = float((LinMessage[2] << 4) + ((LinMessage[1] & 0xF0) >> 4)) / 10.0 - 273;  // Water temperature [degC]
-      Serial_printf("Raumtemperatur-Soll   = %0.1f\n", tRoom);
-      Serial_printf("Wassertemperatur-Soll = %0.1f\n", tWater);
+      if (((LinMessage[0] == 0xAA) && ((LinMessage[1] & 0x0F) == 0x0A)) || LinMessage[0] == 0) {
+        tRoom = 0;
+        Serial_printf("Raumtemperatur-Soll   = Off\n");
+      } else {
+        tRoom = float(LinMessage[0] + ((LinMessage[1] & 0x0F) << 8)) / 10.0 - 273;  // Room temperature [degC]
+        Serial_printf("Raumtemperatur-Soll   = %0.0f\n", tRoom);
+      }
+      if (((LinMessage[2] == 0xAA) && ((LinMessage[1]  & 0xF0) == 0xA0)) || LinMessage[0] == 0) {
+        tWater = 0;
+        Serial_printf("Wassertemperatur-Soll = Off\n");
+      } else {
+        tWater = float((LinMessage[2] << 4) + ((LinMessage[1] & 0xF0) >> 4)) / 10.0 - 273;  // Water temperature [degC]
+        Serial_printf("Wassertemperatur-Soll = %0.0f\n", tWater);
+      }
       Serial_printf("energy_mix    : %02X = %s\n", LinMessage[3], trumaMap(ENERGY_MIX_MAPPING, COUNT(ENERGY_MIX_MAPPING), LinMessage[3]));
       Serial_printf("energy_mode   : %02X = %s\n", LinMessage[4], trumaMap(ENERGY_MODE_MAPPING, COUNT(ENERGY_MODE_MAPPING), LinMessage[4]));
       Serial_printf("energy_mode-2 : %02X = %s\n", LinMessage[5] & 0x0f, trumaMap(ENERGY_MODE_2_MAPPING, COUNT(ENERGY_MODE_2_MAPPING), LinMessage[5] & 0x0f));
@@ -171,20 +191,62 @@ void linDecode() {
   }
 }
 
+uint8_t state = 0;
+
+void linLoop2() {
+  byte ch;
+  if (linSerial.available()) {
+    ch = linSerial.read();
+    // DEBUG__PRINTF("ch1:%02x ", ch);
+    bytes_received++;
+    if (state == 0 && ch == 0) {  // Break  ????
+      state = 1;
+      return;
+    }
+    if (state == 1) {  //  zuvor wurde 00 (break) empfangen
+      if (ch == 0x55) {
+        bytes_received = 2;  // jetzt wird 55 (sync) empfangen
+        state          = 0;
+        Serial.print("\n00 ");
+      } else {                      // nach 00 wurde nicht 55 empfangen --> kein break,sync
+        if (bytes_received > 12) {  // Maximal 12 Zeichen   break+sync+ID+8-Daten+Chk
+          Serial.print("\n");
+          bytes_received = 0;
+        }
+        Serial_printf("00 %02x ", ch);
+        state = 2;
+      }
+    }
+    // DEBUG__PRINTF("bytes_received:%i\n", bytes_received);
+    if (bytes_received > 12) {  // Maximal 12 Zeichen   break+sync+ID+8-Daten+Chk
+      Serial.print("\n");
+      bytes_received = 0;
+    }
+    if (state == 0) Serial_printf("%02x ", ch);
+    if (state == 2) state = 0;
+    return;
+  }
+  return;
+}
+
 void serialCommand() {
   String msg;
+  int tmp;
   if (Serial.available()) {
     msg = Serial.readString();
     //DEBUG__PRINTF("msg:%s", msg.c_str());
     switch (msg[0]) {
-      case 'd':
-        DEBUG__PRINTLN("decode on");
-        decode = true;
+      case 'm':
+        tmp = msg.substring(1).toInt();
+        DEBUG__PRINTF("LogMode:%i\n", tmp);
+        if (tmp >= 0 && tmp <= 2) {
+          logMode = tmp;
+        }
         break;
 
       case 'n':
         DEBUG__PRINTLN("decode off");
-        decode = false;
+        logMode = 1;
         break;
 
       default:
@@ -222,17 +284,4 @@ uint8_t getChecksum(uint8_t ProtectedID, uint8_t dataLen) {
   while (sum >> 8) sum = (sum & 0xFF) + (sum >> 8);
   // inverting result
   return (~sum);
-}
-
-// Hilfsroutine weil Arduino kein Serial.printf hat
-//  https://www.e-tinkers.com/2020/01/do-you-know-arduino-sprintf-and-floating-point/
-//  https://en.cppreference.com/w/cpp/language/parameter_pack
-template <typename... T>
-void Serial_printf(const char* str, T... args) {
-  int len = snprintf(NULL, 0, str, args...);
-  if (len) {
-    char buff[len + 1];
-    snprintf(buff, len + 1, str, args...);
-    Serial.print(buff);
-  }
 }
--- src/truma.h
+++ src/truma.h
@@ -3,26 +3,28 @@
 #define myTRUMA_H
 #include <Arduino.h>
 
+#define trumaMsgLen 16
+
 struct truma {
   uint8_t code;
-  char text[20];
+  char text[16];
 };
 
-truma ENERGY_MIX_MAPPING[] = {     //ID:20  Data:3
+const PROGMEM truma ENERGY_MIX_MAPPING[] = {     //ID:20  Data:3
   {0x00, "electricity"},
   {0xFA, "gas/mix"}};
 
-truma ENERGY_MODE_MAPPING[] = {    //ID:20  Data:4
+const PROGMEM truma ENERGY_MODE_MAPPING[] = {    //ID:20  Data:4
   {0x00, "gas"},
-  {0x09, "mix/electricity 1"},
-  {0x12, "mix/electricity 2"}};
+  {0x09, "mix/electric 1"},
+  {0x12, "mix/electric 2"}};
 
-truma ENERGY_MODE_2_MAPPING[] = {  //ID:20  Data:5&0F
+const PROGMEM truma ENERGY_MODE_2_MAPPING[] = {  //ID:20  Data:5&0F
   {0x01, "Gas"},
-  {0x02, "Electricity"},
-  {0x03, "Gas/Electricity"}};
+  {0x02, "Electric"},
+  {0x03, "Gas/Electric"}};
 
-truma VENT_MODE_MAPPING[] = {      //ID:20  Data:5 >> 4
+const PROGMEM truma VENT_MODE_MAPPING[] = {      //ID:20  Data:5 >> 4
   {0x00, "Off"},
   {0x0B, "Eco"},
   {0x0D, "High"},
@@ -32,7 +34,7 @@ truma VENT_MODE_MAPPING[] = {      //ID:20  Data:5 >> 4
   {0x07, "Vent 7"}, {0x08, "Vent 8"},
   {0x09, "Vent 9"}, {0x0A, "Vent 10"}};
 
-truma VENT_OR_OPERATING_STATUS[] = {  //ID:21(61)  Data:5
+const PROGMEM truma VENT_OR_OPERATING_STATUS[] = {  //ID:21(61)  Data:5
   {0x01, "off"},
   {0x22, "on + airvent"},
   {0x02, "on"},
@@ -40,7 +42,7 @@ truma VENT_OR_OPERATING_STATUS[] = {  //ID:21(61)  Data:5
   {0x32, "fatal error"},
   {0x21, "airvent (?)"}};
 
-truma CP_PLUS_DISPLAY_STATUS_MAPPING[] = {  //ID:22(e2)  Data:1
+const PROGMEM truma CP_PLUS_DISPLAY_STATUS_MAPPING[] = {  //ID:22(e2)  Data:1
   {0xF0, "heating on"},
   {0x20, "standby ac on"},
   {0x00, "standby ac off"},
@@ -49,29 +51,33 @@ truma CP_PLUS_DISPLAY_STATUS_MAPPING[] = {  //ID:22(e2)  Data:1
   {0x50, "boiler on"},
   {0x40, "boiler off"}};
 
-truma HEATING_STATUS_MAPPING[] = {    //ID:22(e2)  Data:2
+const PROGMEM truma HEATING_STATUS_MAPPING[] = {    //ID:22(e2)  Data:2
   {0x10, "boiler eco done"},
-  {0x11, "boiler eco heating"},
+  {0x11, "boiler eco heat"},
   {0x30, "boiler hot done"},
-  {0x31, "boiler hot heating"}};
+  {0x31, "boiler hot heat"}};
 
-truma HEATING_STATUS_2_MAPPING[] = {  //ID:22(e2)  Data:3
+const PROGMEM truma HEATING_STATUS_2_MAPPING[] = {  //ID:22(e2)  Data:3
   {0x04, "normal"},
   {0x05, "error"},
   {0xFF, "fatal error (?)"},
   {0xFE, "normal (?)"}};
 
-char truma_unbekannt[] = "????";
+const char truma_unbekannt[] = "????";
 
+char trumaTmpMsg[trumaMsgLen];
+uint8_t trumaMapCode;
 
-char* trumaMap(truma map[], size_t size, uint8_t code) {
+char* trumaMap(const truma map[], size_t size, uint8_t code) {
   for (size_t i = 0; i < size; i++) {
-    if (map[i].code == code) {
-      //DEBUG__PRINTF("trmaMap - %02x %s\n", code,map[i].text );
-      return map[i].text;
+    memcpy_P(&trumaMapCode, &map[i].code,1);
+    if (trumaMapCode == code) {
+      strcpy_P(trumaTmpMsg,(char *)map[i].text);
+      //DEBUG__PRINTF("trumaMap - %02x %s\n", code,trumaTmpMsg);
+      return trumaTmpMsg;
     }
   }
-  return truma_unbekannt;
+  return (char *)truma_unbekannt;
 }
 
 #endif
